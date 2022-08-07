740d95a64a91c88bd84566b66a30c1bb4ee1b988
dremkay <dremkay71@gmail.com>
2022-08-07 02:09:02 +0100

    life functionality added

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
 const randomNumber = 7
-
+let lives = 3
 
 document.querySelector("#btn1").addEventListener('click',guess)
 document.querySelector("#btn2").addEventListener('click',clear)
 const success = document.querySelector(".success")
 const failed = document.querySelector(".failed")
+const life = document.querySelector("#lives")
 
 
 function guess() {
-    const userInput = Number(document.querySelector("#userInput").value)
-    console.log(`Type: ${typeof userInput}, User guessed: ${userInput}`)
-    if (userInput != randomNumber){
-        success.innerHTML=''
-        if (userInput < randomNumber && userInput!=0){
-            failed.innerHTML = 'Wrong!, too <span id="low-high">low</span>'
-        }else if(userInput === 0){
-            failed.innerHTML = 'Please!, enter a valid number!'
+    if (lives!=0){
+        const userInput = Number(document.querySelector("#userInput").value)
+        console.log(`Type: ${typeof userInput}, User guessed: ${userInput}`)
+        if (userInput != randomNumber){
+            success.innerHTML=''
+            if (userInput < randomNumber && userInput!=0){
+                lives--
+                life.innerHTML=`<h1>${lives}`
+                failed.innerHTML = 'Wrong!, too <span id="low-high">low</span>'
+            }else if(userInput === 0){
+                failed.innerHTML = 'Please!, enter a valid number!'
+            }else{
+                lives--
+                life.innerHTML=`<h1>${lives}`
+                failed.innerHTML = 'Wrong!, too <span id="low-high">high</span>'
+            }
+            
         }else{
-            failed.innerHTML = 'Wrong!, too <span id="low-high">high</span>'
+            failed.innerHTML =''
+            lives--
+            life.innerHTML=`<h1>${lives}`
+            success.innerHTML = `Congratulations, you guessed ${randomNumber} correctly.!`
         }
-        console.log("wrong!")
     }else{
-        failed.innerHTML =''
-        success.innerHTML = `Congratulations, you guessed ${randomNumber} correctly.!`
-        console.log("Congrats!")
+        failed.innerHTML = 'SORRY!, YOU HAVE EXHUSTED YOUR CHANCES! haha!'
+        failed.innerHTML+=`<br/><br/> The answer is ${randomNumber}`
     }
+    
+
 }
 
 function clear(){
