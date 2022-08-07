60c61ddebb23281fab9988d13fca28ce465ceb99
dremkay <dremkay71@gmail.com>
2022-08-07 01:17:17 +0100

    clean up done

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 const randomNumber = 7
 
-const clear =()=> {
-    success.innerHTML =''
-    failed.innerHTML =''
-    document.querySelector("#userInput").value =''
-
-    console.log('claer!')
-}
 
 document.querySelector("#btn1").addEventListener('click',guess)
 document.querySelector("#btn2").addEventListener('click',clear)
@@ -14,21 +7,31 @@ const success = document.querySelector(".success")
 const failed = document.querySelector(".failed")
 
 
-
-
-
 function guess() {
     const userInput = Number(document.querySelector("#userInput").value)
     console.log(`Type: ${typeof userInput}, User guessed: ${userInput}`)
     if (userInput != randomNumber){
         success.innerHTML=''
-        failed.innerHTML = 'Wrong!, too <span id="low-high">low</span>'
+        if (userInput < randomNumber && userInput!=0){
+            failed.innerHTML = 'Wrong!, too <span id="low-high">low</span>'
+        }else if(userInput === 0){
+            failed.innerHTML = 'Please!, enter a valid number!'
+        }else{
+            failed.innerHTML = 'Wrong!, too <span id="low-high">high</span>'
+        }
         console.log("wrong!")
     }else{
         failed.innerHTML =''
-        success.innerHTML = 'Congratulations!'
+        success.innerHTML = `Congratulations, you guessed ${randomNumber} correctly.!`
         console.log("Congrats!")
     }
 }
+
+function clear(){
+    success.innerHTML =''
+    failed.innerHTML =''
+    document.querySelector("#userInput").value =''
+}
+
 // const Game =()=>{
 
